988657fcdf867a1d69f06b2fdfc4e0fdeb9d7caf
Lova Duvnäs <lova.duvnas@yh.nackademin.se>
2020-09-29 09:58:31 +0200

    Updates

---
+++ precache-manifest.e8382bc0051159a53b03d016b28d6739.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f931a598f96b3845cc42e808531eb823",
+    "url": "/js-test-deploy/index.html"
+  },
+  {
+    "revision": "d0e530996b5b3fcf15ca",
+    "url": "/js-test-deploy/static/css/main.6c785126.chunk.css"
+  },
+  {
+    "revision": "068b8deeaffe4c8d7eae",
+    "url": "/js-test-deploy/static/js/2.b32d2641.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/js-test-deploy/static/js/2.b32d2641.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0e530996b5b3fcf15ca",
+    "url": "/js-test-deploy/static/js/main.532afdfb.chunk.js"
+  },
+  {
+    "revision": "a5eb7bf1d09c203dac32",
+    "url": "/js-test-deploy/static/js/runtime-main.cb47ccd2.js"
+  }
+]);
